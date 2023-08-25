71e5325b1478c72748c817a7755e8271fefcc41c
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-08-25 10:50:26 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+def recursive_sum(numbers):
+    '''
+    This function calculates the sum of a list of numbers recursively.
+
+    :param numbers: List of numbers
+    :return: Sum of numbers
+    '''
+    # TODO: Implement the recursive function to calculate the sum of numbers
 
 
 if __name__ == '__main__':
-    calculate()
+    numbers = [5, 3, 9, 1, 7]
+    result = recursive_sum(numbers)
+    print('The sum of numbers is:', result)
--- main_test.py
+++ main_test.py
@@ -1,6 +1,8 @@
 import main
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+def test_recursive_sum():
+    assert main.recursive_sum([5, 3, 9, 1, 7]) == 25
+    assert main.recursive_sum([1, 1, 1, 1, 1, 1]) == 6
+    assert main.recursive_sum([100]) == 100
+    assert main.recursive_sum([]) == 0
+    assert main.recursive_sum([-5, 5, -5, 5]) == 0

dcf857c6eaf162694a5fa04aa8a8854bfe67e52b
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:02:18 +0200

    Ran pyink on branch solution

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 def recursive_sum(numbers):
-    '''
+    """
     This function calculates the sum of a list of numbers recursively.
 
     :param numbers: List of numbers
     :return: Sum of numbers
-    '''
+    """
     # Base case: if the list is empty, return 0
     if len(numbers) == 0:
         return 0
 
     # Recursive case: add the first number to the sum of the rest of the list
     return numbers[0] + recursive_sum(numbers[1:])
+
 
 if __name__ == '__main__':
     numbers = [5, 3, 9, 1, 7]
